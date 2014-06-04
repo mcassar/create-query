b442b912180158550e1d0c3688d14afb9f346cd8
mcassar <mark.cassar@me.com>
2014-06-04 13:43:11 +1000

    Update create-query.ps1

--- create-query.ps1
+++ create-query.ps1
@@ -1,11 +1,11 @@
-function create-query ([string]$database,[string]$dir) 
+function create-query () 
 {
-
-$csv = import-csv $database 
-
-foreach ($i in $csv )
+    param([string]$database, [string]$dir);
+    $csv = import-csv $database 
+    foreach ($i in $csv )
     { 
-    write-host  "CREATE DATABASE $($i.db_name) ON (FILENAME = $($dir)\$($i.db_name).mdf), (FILENAME = $($dir)\$($i.db_name).ldf) FOR ATTACH;`n"
-   }
-  }
+        write-host  "CREATE DATABASE $($i.db_name) ON (FILENAME = $($dir)\$($i.db_name).mdf), (FILENAME = $($dir)\$($i.db_name).ldf) FOR ATTACH;`n"
+    }
+   
+}
 
