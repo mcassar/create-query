2730f258813d56d50f02b5cb9a38744131a39160
mcassar <mark.cassar@me.com>
2014-06-04 14:25:48 +1000

    Update create-query.ps1

--- create-query.ps1
+++ create-query.ps1
@@ -1,11 +1,10 @@
-function create-query () 
+param([string]$database, [string]$dir);
+
+function create-query ([string]$database,[string]$dir) 
 {
-    param([string]$database, [string]$dir);
     $csv = import-csv $database 
     foreach ($i in $csv )
     { 
         write-host  "CREATE DATABASE $($i.db_name) ON (FILENAME = $($dir)\$($i.db_name).mdf), (FILENAME = $($dir)\$($i.db_name).ldf) FOR ATTACH;`n"
     }
 }
-
-create-query
